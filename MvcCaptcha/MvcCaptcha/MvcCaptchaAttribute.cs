91db5ab45a5395b5d3cfa1e27e9484ed65b9fd8d
Vijaya Anand <vijay.prideparrot@gmail.com>
2013-01-06 22:26:37 +0530

    first checkin with sourcecode

---
+++ MvcCaptcha/MvcCaptcha/MvcCaptchaAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+
+namespace ImageDraw
+{
+  public class MvcCaptchaAttribute : ActionFilterAttribute, IActionFilter
+  {
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+      var captchaActual = filterContext.HttpContext.Request.Form["CaptchaActual"];
+
+      if (captchaActual == "")
+      {
+        filterContext.Controller.ViewData.ModelState.AddModelError("CaptchaActual", "Captcha is required.");
+        return;
+      }
+
+      var captchaExpected = Encoding.Unicode.GetString(Convert.FromBase64String(filterContext.HttpContext.Request.Form["CaptchaExpected"]));
+
+      if (captchaActual != captchaExpected)
+        filterContext.Controller.ViewData.ModelState.AddModelError("CaptchaActual", "The numeric answer for your captcha is invalid");
+    }
+  }
+}
